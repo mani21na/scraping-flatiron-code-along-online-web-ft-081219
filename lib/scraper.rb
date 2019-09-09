01f9073fba322fdb47b22e10c3302859bad5a9d4
Hanna Lee <mani21na@gmail.com>
2019-09-09 19:18:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,8 @@ class Scraper
   end
   
   def get_page
-    html = open("http://flatironschool.com/")
+    html = Nokogiri::HTML(open("http://flatironschool.com/"))
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
 end
